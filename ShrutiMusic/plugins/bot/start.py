ad4d6ae535070f2fea8d018a1a2f3289eb767dfd
Shadow737hub <nothinnothin162@gmail.com>
2025-08-12 17:00:31 +0530

    Update start.py

--- ShrutiMusic/plugins/bot/start.py
+++ ShrutiMusic/plugins/bot/start.py
@@ -169,14 +169,3 @@ async def welcome(client, message: Message):
         except Exception as ex:
             print(ex)
 
-
-# ©️ Copyright Reserved - @NoxxOP  Nand Yaduwanshi
-
-# ===========================================
-# ©️ 2025 Nand Yaduwanshi (aka @NoxxOP)
-# 🔗 GitHub : https://github.com/NoxxOP/ShrutiMusic
-# 📢 Telegram Channel : https://t.me/ShrutiBots
-# ===========================================
-
-
-# ❤️ Love From ShrutiBots 
