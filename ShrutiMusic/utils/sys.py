096bc14d939cd43a80b12406b0de6225f3664c99
Shadow737hub <nothinnothin162@gmail.com>
2025-08-15 23:00:56 +0530

    Update sys.py

--- ShrutiMusic/utils/sys.py
+++ ShrutiMusic/utils/sys.py
@@ -4,8 +4,8 @@ import time
 
 import psutil
 
-from ShrutiMusic.misc import _boot_
-from ShrutiMusic.utils.formatters import get_readable_time
+from IVANMUSIC.misc import _boot_
+from IVANMUSIC.utils.formatters import get_readable_time
 
 
 async def bot_sys_stats():
